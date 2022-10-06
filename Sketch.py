89f952449cfa9e55020a1ff81e302fe2103e2ef0
Kilian Voss <kilian96@live.de>
2022-10-06 17:18:31 +0200

    basic structure of the repository defined

---
+++ Sketch.py
@@ -0,0 +1,11 @@
+
+
+# Integration of the Frontend Abelian Terminal
+
+# Integaration of Abelian Clae
+
+# Integration of Abelian Models
+
+# Integration of Abelian Backtesting
+
+# Integration of Abelian LiveTrading
