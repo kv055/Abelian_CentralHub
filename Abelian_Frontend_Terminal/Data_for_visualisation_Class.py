497f65180489f6e4d6b0fabc936b9448f60920f2
Kilian Voss <kilian96@live.de>
2022-12-10 21:04:42 -0500

    return_Indicators_data methood working

--- Abelian_Frontend_Terminal/Data_for_visualisation_Class.py
+++ Abelian_Frontend_Terminal/Data_for_visualisation_Class.py
@@ -9,13 +9,11 @@ class Plot_for_Terminal:
     def return_OHLC_data(self, asset_dict):
         # Fetch Assets DB by ticker and data_provider
         self.ohlc_set = self.ohlc_db.return_historical_ohlc_from_db(asset_dict)
-        
-        # create indicators_instance
-        self.indicator_instance = Indicators(self.ohlc_set)
-
         return self.ohlc_set
 
-    def return_Indicators_data(self, indicator_name):
-        # Get args by indicator_name
-        # self.indicator_instance.
-        pass
+    def return_Indicators_data(self, indicator_config_dict):
+        # create indicators_instance
+        self.indicator_instance = Indicators(self.ohlc_set)
+        selected_indicator_method = getattr(self.indicator_instance, indicator_config_dict['Name'])
+        Indicator = selected_indicator_method(indicator_config_dict['Range'])
+        return Indicator
